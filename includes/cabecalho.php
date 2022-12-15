210339474c4d53aa33b2f9d58adca73075dd6d2b
Paola Biscaro <paolabiscaroper@gmail.com>
2022-12-15 10:18:29 -0300

    html do cabeçalho

---
+++ includes/cabecalho.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Make Planner</title>
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<header>
+    <nav>
+        <div>
+            <img src="/assets/img/logo.png">
+        </div>
+        <div>
+            <ul>
+                <li><a href="/index.php"><i class="fa-solid fa-house"></i>Home</a></li>
+                <li><a href="/admin/modelos.php"><i class="fa-solid fa-clipboard"></i>Modelos</a></li>
+                <li><a href="/admin/meusPlanners.php"><i class="fa-solid fa-thumbtack"></i>Meus Planners</a></li>
+                <li><a href="/admin/novoPlanner.php"><i class="fa-solid fa-pen-to-square"></i>Novo Planner</a></li>
+                <li><a href="/admin/configuracoes.php"><i class="fa-solid fa-gear"></i>Configurações</a></li>
+                <li><a href="/admin/ajuda.php"><i class="fa-regular fa-circle-question"></i>Ajuda</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li><i class="fa-solid fa-right-to-bracket"></i>Log out</li>
+            </ul>
+
+        </div>
+    </nav>
+    <nav>
+        <div>
+            <label></label>
+            <i class="fa-solid fa-magnifying-glass"></i><input type="text" placeholder="Pesquisar...">
+        </div>
+        <div>
+            <img>
+                <p>Nome Usuario <br>
+                    email@email.com</p>
+                    <i class="fa-solid fa-bell-slash"></i>
+                    <i class="fa-solid fa-bell"></i>
+        </div>
+    </nav>
+</header>
